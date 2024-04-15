127489ae8e1113b19566475eade9c1c8cf84bd23
Austin Klein <austinkl@usc.edu>
2024-04-14 21:27:01 -0400

    Add WCON support for viewer, add perimeter support, and use Numpy and JSON libraries for WCON generation

--- WormSim/Model/WormView.py
+++ WormSim/Model/WormView.py
@@ -1,24 +1,8 @@
 from matplotlib import pyplot as plt
-
 from numpy import genfromtxt
 import numpy as np
-import math
+import json
 import os
-
-data = genfromtxt("simdata.csv", delimiter=",").T
-
-print("Loaded data: %s" % (str(data.shape)))
-
-t = data[0]
-
-x_offset = 1
-y_offset = 2
-d_offset = 3
-
-"""
-for i in [(j*3)+y_offset for j in range(49)]:
-    plt.plot(t,my_data[i],label=i)
-plt.legend()"""
 
 fig, ax = plt.subplots()
 plt.get_current_fig_manager().set_window_title("2D WormSim replay")
@@ -38,82 +22,40 @@ else:
     print("No objects found")
 
 num_t = 30
-timesteps = len(t)
 
-# Using same variable names as WormView.m
-Sz = len(data)
-Nt = len(data[0])
-Nbar = int((Sz - 1) / 3)
-NSEG = int(Nbar - 1)
-D = 80e-6
+with open("simdata.wcon", 'r') as f:
+    wcon = json.load(f)
 
-R = [
-    D / 2.0 * abs(math.sin(math.acos(((i) - NSEG / 2.0) / (NSEG / 2.0 + 0.2))))
-    for i in range(Nbar)
-]
+t = np.array(wcon["data"][0]["t"])
+x = np.array(wcon["data"][0]["x"]).T
+y = np.array(wcon["data"][0]["y"]).T
+px = np.array(wcon["data"][0]["px"]).T
+py = np.array(wcon["data"][0]["py"]).T
 
-CoM = np.zeros([Nt, Nbar, 3])
-CoMplot = np.zeros([Nt, 2])
-Dorsal = np.zeros([Nbar, 2])
-Ventral = np.zeros([Nbar, 2])
+timesteps = t.size
 
-print(f"Sz: {Sz}, Nt: {Nt}, Nbar: {Nbar}, NSEG: {NSEG}")
-# Dt = data(2,1) - data(1,1);
-
-ventral_plot = None
 midline_plot = None
-dorsal_plot = None
+perimeter_plot = None
 
 from Player import Player
 
 
 def update(ti):
-    global dorsal_plot, ventral_plot, midline_plot
+    global midline_plot, perimeter_plot
     f = ti / timesteps
 
     color = "#%02x%02x00" % (int(0xFF * (f)), int(0xFF * (1 - f) * 0.8))
     print("Time step: %s, fract: %f, color: %s" % (ti, f, color))
-    ds = []
-    xs = []
-    ys = []
 
-    for i in [(j * 3) + d_offset for j in range(Nbar)]:
-        ds.append(data[i][ti])
-
-    for i in [(j * 3) + x_offset for j in range(Nbar)]:
-        xs.append(data[i][ti])
-
-    for i in [(j * 3) + y_offset for j in range(Nbar)]:
-        ys.append(data[i][ti])
-
-    for j in range(Nbar):
-        dX = R[j] * math.cos(ds[j])
-        dY = R[j] * math.sin(ds[j])
-
-        Dorsal[j, 0] = xs[j] + dX
-        Dorsal[j, 1] = ys[j] + dY
-        Ventral[j, 0] = xs[j] - dX
-        Ventral[j, 1] = ys[j] - dY
-
-    if dorsal_plot == None:
-        (dorsal_plot,) = ax.plot(Dorsal[:, 0], Dorsal[:, 1], color="grey", linewidth=1)
+    if midline_plot is None:
+        (midline_plot,) = ax.plot(x[:, ti], y[:, ti], color="g", label="t=%sms" % t[ti], linewidth=0.5)
     else:
-        dorsal_plot.set_data(Dorsal[:, 0], Dorsal[:, 1])
-
-    if ventral_plot == None:
-        (ventral_plot,) = ax.plot(
-            Ventral[:, 0], Ventral[:, 1], color="grey", linewidth=1
-        )
+        midline_plot.set_data(x[:, ti], y[:, ti])
+    
+    if perimeter_plot is None:
+        (perimeter_plot,) = ax.plot(px[:, ti], py[:, ti], color="grey", linewidth=1)
     else:
-        ventral_plot.set_data(Ventral[:, 0], Ventral[:, 1])
-
-    if midline_plot == None:
-        (midline_plot,) = ax.plot(
-            xs, ys, color="g", label="t=%sms" % t[ti], linewidth=0.5
-        )
-    else:
-        midline_plot.set_data(xs, ys)
-
+        perimeter_plot.set_data(px[:, ti], py[:, ti])
 
 ax.plot()  # Causes an autoscale update.
 
