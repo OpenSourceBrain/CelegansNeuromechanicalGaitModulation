f72f1a04ae1ed207aff3e597493de5def6d62bce
pgleeson <p.gleeson@gmail.com>
2025-05-06 16:43:05 +0100

    Add test_all scrpt

---
+++ WormSim/Model/test_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+quick_test=0
+
+if [[ ($# -eq 1) && ($1 == '-q') ]]; then
+    quick_test=1
+fi
+
+make clean
+make 
+        
+time ./program
+
+python WormView.py -nogui 
+
